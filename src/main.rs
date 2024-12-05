4de3a7eaa0674380c831f52bd74c896e8add832c
zer0 <max.hasselwander@wiss-edu.ch>
2024-12-05 14:30:56 +0100

    updated main getting started

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use doryen_rs::{App, AppOptions, Console, Input};
 
-fn main() {
-    println!("Hello, world!");
+const SCREEN_WIDTH: u32 = 80;
+const SCREEN_HEIGHT: u32 = 50;
+const FONT_PATH: &str = "./arial10x10.png";
+
+struct Gameboard {
+    fps: u32,
+}
+
+impl doryen_rs::App for Gameboard {
+    fn initialize(&mut self, _console: &mut dyn Console) {}
+    fn update(&mut self, _console: &mut dyn Console, _input: &Input) {}
+    
 }
